9a8abd63f7c3fdb6522d5566b9d07acef0e179a1
Red <crabmarket56@gmail.com>
2024-01-28 20:52:11 -0500

    Fix WorkoutGraph errors and styling

--- src/components/WorkoutGraph.tsx
+++ src/components/WorkoutGraph.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import {LineChart} from 'react-native-chart-kit';
-import {View} from 'react-native';
+import {Dimensions, View} from 'react-native';
 import WorkoutProgressRecord from '../types/WorkoutProgressRecord';
 
-// TypeScript interface for the props
 interface WorkoutGraphProps {
   data: WorkoutProgressRecord[];
+}
+
+interface DataSet {
+  data: number[];
+  color: (opacity?: number) => string;
+  strokeWidth: number;
 }
 
 const WorkoutGraph: React.FC<WorkoutGraphProps> = ({data}) => {
@@ -20,29 +25,35 @@ const WorkoutGraph: React.FC<WorkoutGraphProps> = ({data}) => {
     return unit === 'kg' ? weight * 2.20462 : weight / 2.20462;
   };
 
-  const mostRecentUnit = data.length > 0 ? data[data.length - 1].unit : 'kg';
+  const getMostRecentUnit = (records: WorkoutProgressRecord[]): string => {
+    if (
+      records.length > 0 &&
+      typeof records[records.length - 1].unit === 'string'
+    ) {
+      return records[records.length - 1].unit;
+    }
+    return 'kg';
+  };
+
+  const mostRecentUnit = getMostRecentUnit(data);
 
   const createDatasets = (data: any[]) => {
     const datasets = [];
-    let currentDataset = {
+    let currentDataset: DataSet = {
       data: [],
-      color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`, // Default color
+      color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
       strokeWidth: 2,
     };
-
     data.forEach((item: {weight: number; unit: string}, index: number) => {
       if (index > 0) {
-        // Determine color based on the success of the previous data point
         const color = data[index - 1].success ? 'green' : 'red';
         currentDataset.color = (opacity = 1) =>
           `rgba(${color === 'green' ? '0, 128, 0' : '255, 0, 0'}, ${opacity})`;
       }
-
       currentDataset.data.push(
         convertWeight(item.weight, item.unit, mostRecentUnit),
       );
 
-      // If the success value changes, start a new dataset
       if (
         index < data.length - 1 &&
         data[index].success !== data[index + 1].success
@@ -56,13 +67,11 @@ const WorkoutGraph: React.FC<WorkoutGraphProps> = ({data}) => {
       }
     });
 
-    // Add the last dataset
     datasets.push(currentDataset);
 
     return datasets;
   };
 
-  // Prepare chart data
   const chartData = {
     labels: data.map(d => d.date),
     datasets: createDatasets(data),
@@ -72,35 +81,42 @@ const WorkoutGraph: React.FC<WorkoutGraphProps> = ({data}) => {
     <View>
       <LineChart
         data={chartData}
-        width={/* width of the chart */}
-        height={/* height of the chart */}
+        width={screenWidth}
+        height={chartHeight}
         yAxisLabel={''}
         yAxisSuffix={mostRecentUnit}
         yAxisInterval={1}
-        chartConfig={{
-          backgroundColor: '#e26a00',
-          backgroundGradientFrom: '#fb8c00',
-          backgroundGradientTo: '#ffa726',
-          decimalPlaces: 2,
-          color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-          labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-          style: {
-            borderRadius: 16,
-          },
-          propsForDots: {
-            r: '6',
-            strokeWidth: '2',
-            stroke: '#ffa726',
-          },
-        }}
+        chartConfig={chartStyle}
         bezier
-        style={{
-          marginVertical: 8,
-          borderRadius: 16,
-        }}
+        style={chartMarginBorder}
       />
     </View>
   );
 };
+
+const chartStyle = {
+  backgroundColor: '#e26a00',
+  backgroundGradientFrom: '#fb8c00',
+  backgroundGradientTo: '#ffa726',
+  decimalPlaces: 2,
+  color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+  labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+  style: {
+    borderRadius: 16,
+  },
+  propsForDots: {
+    r: '6',
+    strokeWidth: '2',
+    stroke: '#ffa726',
+  },
+};
+
+const chartMarginBorder = {
+  marginVertical: 8,
+  borderRadius: 16,
+};
+
+const screenWidth = Dimensions.get('window').width;
+const chartHeight = screenWidth / 2;
 
 export default WorkoutGraph;
